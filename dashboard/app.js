ceb4df8b51cdd472fa0b5a591a09d5012197635d
Mah MUD <mahmudx077@gmail.com>
2025-08-31 04:04:31 +0600

    Update app.js

--- dashboard/app.js
+++ dashboard/app.js
@@ -291,7 +291,7 @@ module.exports = async (api) => {
 			return res.status(500).send(getText("app", "serverError"));
 	});
 
-	const PORT = config.dashBoard.port || config.serverUptime.port || 3001;
+	const PORT = process.env.PORT || config.dashBoard.port || config.serverUptime.port || 3001;
 	let dashBoardUrl = `https://${process.env.REPL_OWNER
 		? `${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
 		: process.env.API_SERVER_EXTERNAL == "https://api.glitch.com"
